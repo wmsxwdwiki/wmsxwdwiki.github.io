506171d8a0a1557a13da903335f63f72973865cc
Jokns <jokns@wmsxwd.com>
2020-06-06 07:56:39 +0000

    update

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+sudo apt -y install jq git file nodejs build-essential
+sudo npm install -g gitbook-cli
+
+gitbook init
+gitbook install
+gitbook build
+
+TARGET_DIR=_wiki_wmsxwd_com
+
+git clone "https://github.com/wmsxwdwiki/wmsxwdwiki.github.io.git" ${TARGET_DIR}
+
+rm -rf ${TARGET_DIR}/*
+cp -r ./_book/* ${TARGET_DIR}/
+#cp CNAME ${TARGET_DIR}/
+#cp robots.txt ${TARGET_DIR}/
+#cp -r ./resources/* ${TARGET_DIR}/resources/
+
+pushd ${TARGET_DIR}
+git config user.name "Jokns"
+git config user.email "jokns@wmsxwd.com"
+
+git add -A
+git commit -m 'update'
+git push "https://9ed4d03bf102d55941361eca50c33a91a5a5bf75@github.com/wmsxwdwiki/wmsxwdwiki.github.io.git" master
+popd
